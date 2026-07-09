32e5c46f60f761626a791575a8a43af6aee27a17
Evgenii Ikonnikov <78169095+JRaketa@users.noreply.github.com>
2026-07-09 17:14:15 +0500

    Update sessions.py

--- scripts/sessions.py
+++ scripts/sessions.py
@@ -5,6 +5,7 @@ from google import genai
 from pydantic import TypeAdapter
 from google.genai import types
 from pprint import pprint
+from uuid import uuid4
 #crew_llm = os.getenv("MAX_HISTORY_MESSAGES")
 #system_prompt = os.getenv("GEMINI_SYSTEM_INSTRUCTION")
 
@@ -21,8 +22,12 @@ class ChatSessionManager:
         self._sessions: Dict[str, dict] = {}
         # { user_id : building_id}
         self._current_context: Dict[str, dict] = {}
+        # { user_id : list(building_id)}
+        self._user_contexts_registered: Dict[str, list] = {}
         # { user_id : {building_id: gemini_chat}}
         self._chats: Dict[str, Dict[str, client.chats]] = {}
+        # { user_id : {building_id: {subsession_id: gemini_chat}}}
+        self._subsession: Dict[str, Dict[str, Dict[str, client.chats]]] = {}
         # { building_id : context}
         self._contexts: Dict[str, Dict[str, str]] = {}
         # { building_id : file_url}
@@ -34,14 +39,6 @@ class ChatSessionManager:
         self.context_folder_path = os.getenv("CONTEXTS_FOLDER")
         self.history_adapter = TypeAdapter(list[types.Content])
 
-    def save_context_as_txt(
-        self, building_id: str,
-        ai_context: str, cadastrial_context: str):
-        """."""
-        with open(self.get_ai_context_file_path(building_id), "w") as f:
-            f.write(ai_context)
-        with open(self.get_cadastrial_context_file_path(building_id), "w") as f:
-            f.write(cadastrial_context)
 
     def get_ai_context_file_path(self, building_id:str):
         return self.context_folder_path + "_ai_context_" + building_id + ".txt"
@@ -61,6 +58,172 @@ class ChatSessionManager:
             file=cadastrial_context_path)
         return ai_context_url, cadastrial_context_url
 
+    #def init_subsession(sessions, user_id, building_id, subsession_id):
+        # Создаём вложенность, если её нет: user_id -> {}
+#        user_dict = sessions.setdefault(user_id, {})
+
+        # Создаём/получаем словарь building_id внутри user_id
+#        building_dict = user_dict.setdefault(building_id, {})
+
+        # Добавляем subsession_id (если такой уже есть — перезапишет, если нужно только при отсутствии, см. ниже)
+#        building_dict[subsession_id] = self.get_new_chat()
+
+#        return sessions
+
+    def update_subsession(
+        self, user_id: str,
+        building_id: str, subsession_id: str
+        ):
+        """."""
+
+        user_sessions = self._subsession.get(user_id)
+        if user_sessions != None:
+            user_subsessions = user_sessions.get(building_id)
+            if user_subsessions != None:
+                self._subsession[user_id][building_id][subsession_id] = self.get_new_chat()
+            else:
+                self._subsession.update({
+                    user_id: {
+                        building_id: {
+                            subsession_id: self.get_new_chat()
+                            }
+                        }
+                    })
+        else:
+            self._subsession.update({
+                user_id: {
+                    building_id: {
+                        subsession_id: self.get_new_chat()
+                        }
+                    }
+                })
+
+
+    def init_subsession(self, user_id: str, building_id: str):
+        # { user_id : {building_id: {subsession_id: gemini_chat}}}
+        subsession_id = str(uuid4())
+
+        self.update_subsession(
+            user_id, building_id, subsession_id
+            )
+        return subsession_id
+
+
+    def get_user_subsessions_report(self, user_subsessions):
+        buildings_ids_list = list(user_subsessions.keys())
+        report_dict = {}
+        for buildings_id in buildings_ids_list:
+            subsessions_list = list(user_subsessions.get(buildings_id).keys())
+            report_dict[buildings_id] = subsessions_list
+        return report_dict
+
+
+    def get_user_subsessions(self, user_id):
+        user_subsessions = self._subsession.get(user_id)
+        if user_subsessions == None:
+            return {
+                "status": "error",
+                "user_id": user_id,
+                "user_subsessions": [],
+                "comment": "subsessions list is empty"
+            }
+        user_subsessions_list = list(user_subsessions.keys())
+        return {
+            "status": "success",
+            "user_id": user_id,
+            "user_subsessions": user_subsessions_list,
+            "comment": ""
+        }
+
+
+    def no_subsession_responce(self, user_id, building_id):
+        return {
+            "status": "error",
+            "user_id": user_id,
+            "building_id": building_id,
+            "comment": "no subsessions registered"}
+
+    def subsessions_list_responce(
+        self, user_id,
+        building_id, building_sub):
+        return {
+            "user_id": user_id,
+            "building_id": building_id,
+            "subsessions_list": list(building_sub.keys())}
+
+    def get_subsessions_list(self, user_id, building_id):
+        user_sub = self._subsession.get(user_id)
+        if user_sub != None:
+            building_sub = user_sub.get(building_id)
+            if building_sub != None:
+                if list(building_sub.keys()):
+                    return self.subsessions_list_responce(
+                        user_id, building_id, building_sub)
+                else:
+                    return self.no_subsession_responce(user_id, building_id)
+            else:
+                return self.no_subsession_responce(user_id, building_id)
+        else:
+            return self.no_subsession_responce(user_id, building_id)
+
+    def init_chat(self, user_id, building_id):
+        self._chats[user_id] = {building_id: self.get_new_chat()}
+
+
+    def get_new_chat(self):
+        return self.client.chats.create(
+            model=self.model,
+            config=types.GenerateContentConfig(
+                system_instruction=self.system_prompt
+                )
+            )
+
+    def init_user_buildings_registries(
+        self, user_id:str, building_id:str
+        ):
+        self._user_contexts_registered.update({
+            user_id: [building_id]
+            })
+
+    def get_user_buildings_ids(self, user_id):
+        if self._user_contexts_registered.get(user_id) == None:
+            return []
+        return self._user_contexts_registered.get(user_id)
+
+    def get_registered_contexts(self):
+        return list(self._contexts.keys())
+
+
+    def add_building_to_user_context(
+        self, user_id:str, building_id:str
+        ):
+        users_building_ids = self.get_user_buildings_ids(user_id)
+        users_building_ids.append(building_id)
+        self._user_contexts_registered.update({
+            user_id: users_building_ids})
+
+    def update_user_contexts(
+        self, user_id:str, building_id:str
+        ):
+        users_building_ids = self._user_contexts_registered.get(user_id)
+        if users_building_ids == None:
+            self.init_user_buildings_registries(user_id, building_id)
+            return False
+        if building_id in users_building_ids:
+            return True
+        else:
+            return False
+
+    def save_context_as_txt(
+        self, building_id: str,
+        ai_context: str, cadastrial_context: str):
+        """."""
+        with open(self.get_ai_context_file_path(building_id), "w") as f:
+            f.write(ai_context)
+        with open(self.get_cadastrial_context_file_path(building_id), "w") as f:
+            f.write(cadastrial_context)
+
+
     def update_context(
         self, building_id:str,
         ai_context: str, cadastrial_context: str
@@ -78,42 +241,45 @@ class ChatSessionManager:
         ai_context:str, cadastrial_context:str
         ):
         """."""
-        self.update_context(
-            building_id, ai_context, cadastrial_context)
-        self.save_context_as_txt(
-            building_id, ai_context, cadastrial_context
-            )
-        #file_path = self.get_context_file_path(building_id)
-        ai_context_url, cadastrial_context_url = self.load_context_to_vec_store(building_id)
-        self._contexts_url_files[building_id] = [
-            ai_context_url, cadastrial_context_url
-            ]
-
-    def init_chat(self, user_id, building_id):
-        self._chats[user_id] = {building_id: self.get_new_chat()}
-
-    def get_user_contexts(self, user_id):
-        user_chats = self._chats.get(user_id)
-        if user_chats != None:
-            return list(user_chats.keys())
-        return None
-
-
-    def get_new_chat(self):
-        return self.client.chats.create(
-            model=self.model,
-            config=types.GenerateContentConfig(
-                system_instruction=self.system_prompt
+        if building_id not in self.get_registered_contexts():
+            self.update_context(
+                building_id, ai_context, cadastrial_context)
+            self.save_context_as_txt(
+                building_id, ai_context, cadastrial_context
                 )
-            )
+            ai_context_url, cadastrial_context_url = self.load_context_to_vec_store(building_id)
+            self._contexts_url_files[building_id] = [
+                ai_context_url, cadastrial_context_url
+                ]
 
-    def init_session(self, user_id:str, building_id:str,
-        ai_context: str, cadastrial_context: str):
-
+    def init_session(
+        self, user_id:str, building_id:str,
+        ai_context: str, cadastrial_context: str
+        ):
         """."""
         self.add_context(
             building_id, ai_context, cadastrial_context)
-        self.init_chat(user_id, building_id)
+
+        if building_id in self.get_user_buildings_ids(user_id):
+            return {
+                "status": "error",
+                "user_id": user_id,
+                "building_id": building_id,
+                "comment": (
+                    f"context for pair user_id {user_id} and "
+                    f"building_id {building_id} has already been registered")
+                }
+        self.add_building_to_user_context(user_id, building_id)
+        subsession_id = self.init_subsession(
+            user_id, building_id)
+        return {
+            "status": "success",
+            "user_id": user_id,
+            "building_id": building_id,
+            "subsession_id": subsession_id
+        }
+
+
         self._current_context.update({user_id: building_id})
 
     def get_context(self, building_id:str):
@@ -126,11 +292,17 @@ class ChatSessionManager:
     def get_current_context_id(self, user_id):
         return self._current_context.get(user_id, None)
 
-    def get_chat(self, user_id, building_id):
-        chat = self._chats.get(user_id, None)
-        if chat == None:
+    def get_chat(
+        self, user_id: str,
+        building_id: str, subsession_id: str
+        ):
+        user_sessions = self._subsession.get(user_id)
+        if user_sessions == None:
             return None
-        chat = chat.get(building_id, None)
+        user_subsessions = user_sessions.get(building_id)
+        if user_subsessions == None:
+            return None
+        chat = user_subsessions.get(subsession_id)
         if chat == None:
             return None
         return chat
@@ -154,8 +326,13 @@ class ChatSessionManager:
             history.append(self.extract_replica_from_parts(msg))
         return history
 
-    def get_history(self, user_id, building_id):
-        chat = self.get_chat(user_id, building_id)
+    def get_history(
+        self, user_id: str,
+        building_id: str, subsession_id: str
+        ):
+        chat = self.get_chat(
+            user_id, building_id, subsession_id
+            )
         return self.extract_history(chat)
 
     def create_parts(self, files_urls):
@@ -170,11 +347,11 @@ class ChatSessionManager:
                         ))
         return all_parts
 
-    def request_to_llm(self, question, user_id, building_id):
+    def request_to_llm(self, question, user_id, building_id, subsession_id):
         #context = self.get_user_context(user_id)
         #user_prompt = self.make_user_prompt(question, context)
-        chat = self.get_chat(user_id, building_id)
-
+        chat = self.get_chat(user_id, building_id, subsession_id)
+        print("chat:", chat)
         if chat == None:
             return {"status": "error", "Comment": f"The user chat with user_id '{user_id}' and building_id '{building_id}' has not been created!"}
 
@@ -188,12 +365,18 @@ class ChatSessionManager:
         all_parts = self.create_parts(files_urls)
         all_parts.append(question)
         chat.send_message(all_parts)
-
-        self._chats.update({user_id: {building_id: chat}})
-        return {"status": "success"}
+        print("chat:", chat)
+        self._subsession.update({
+            user_id: {
+                building_id: {
+                    subsession_id: chat
+                }
+            }
+        })
+#        return {"status": "success"}
 
     def get_users_ids(self):
-        return list(self._current_context.keys())
+        return list(self._subsession.keys())
 
     def get_history_json(self, user_id, building_id):
         chat_history = self.get_history(user_id, building_id)
@@ -203,3 +386,10 @@ class ChatSessionManager:
                 "comment": f"history is empty for user_id: {user_id} and building_id: {building_id}"
                 }
         return self.history_adapter.dump_json(chat_history)
+
+#    def send_mgs_to_llm(self, user_id, building_id, user_request):
+#        chat_history = self.get_history(user_id, building_id)
+#        if chat_history == None:
+#            return {"status": "fail", "comment": f"chat was not initiated for user {user_id}"}
+#        request_text = ''
+#        response = chat.send_message()
